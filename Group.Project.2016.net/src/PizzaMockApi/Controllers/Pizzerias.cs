2ae06170625fd50a394e13f05232cb31666835eb
unknown <patrykm@HEXEN.kainos.com>
2016-10-18 14:19:58 +0200

    added models and mock data for pizzerias

---
+++ Group.Project.2016.net/src/PizzaMockApi/Controllers/Pizzerias.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PizzaModels.Models;
+using PizzaMockApi.MockData;
+
+namespace PizzaMockApi.controllers
+{
+    [Route("api/[controller]")]
+    public class Pizzerias : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Pizzeria> Get()
+        {
+            return PizzeriasData.GetPizzeriasMockData();
+        }
+    }
+}
---
+++ Group.Project.2016.net/src/PizzaModels/Enums/Ingredients.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaModels.Enums
+{
+    public enum Ingredients
+    {
+        TomatoSauce = 1,
+        Cheese = 2,
+        Ham = 3,
+        Onion = 4
+    }
+}
